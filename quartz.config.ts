3dd7e24fd93170d64619767bfa183161aca92d45
SyaoranLord <anhtuan1477@gmail.com>
2026-02-08 18:25:50 -0700

    Quartz sync: Feb 8, 2026, 6:25 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "syaoranlord.github.io",
+    baseUrl: "tuanng.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
